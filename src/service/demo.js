4bc41fa2606a7286739646fe57ab91b04f12b1cb
wgzhao <wgzhao@gmail.com>
2024-01-11 08:45:43 +0800

    Add calc function to demo.js and update Demo.vue to display the sum of 1 + 2

---
+++ src/service/demo.js
@@ -0,0 +1,6 @@
+const calc = (a, b) => {
+    return a + b;
+}
+
+export default {calc}
+
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,53 +1,12 @@
 <template>
   <div id="app">
-    <!-- <apexchart width="500"  :options="options" :series="series"></apexchart> -->
+    1 + 2 = {{ s }}
   </div>
 </template>
 
 <script>
-// import VueApexCharts from "vue3-apexcharts";
-import axios from "axios";
-export default {
-  data() {
-    return {
-      options: {
-        chart: {
-          id: 'vuechart-example'
-        },
-        xaxis: {
-          type: "category",
-          categories: []
-        },
-        stroke: {
-          curve: 'smooth'
-        }
-      },
-      series: [{
-        name: 'series-1',
-        type: "area",
-        data: []
-      }]
-    }
-  },
-  // components: {
-  //   apexchart: VueApexCharts,
-  // },
-  mounted() {
-    this.fetchData()
-    console.log(this.series[0]["data"]);
-    console.log(this.series);
-  },
-  methods: {
-    fetchData() {
-      axios.get("/dashboard/last12MonthsEtlData")
-          .then(res => {
-            for (let i=0; i< res.data.length; i++) {
-              this.series[0]["data"].push(res.data[i]["num"]);
-              this.options.xaxis.categories.push(res.data[i]["month"]);
-            }
-          })
-          .catch(err => console.log(err));
-  }
-  }
-}
+import {ref} from 'vue'
+import demo from '@/service/demo'
+const s = ref(0)
+s.value = demo.calc(1,2);
 </script>
