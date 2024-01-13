ea91bc47f017197aacef31997d3285d17d04b97b
wgzhao <wgzhao@gmail.com>
2024-01-13 22:58:14 +0800

    use Composition API

--- src/views/maintable/MbDataSource.vue
+++ src/views/maintable/MbDataSource.vue
@@ -7,7 +7,7 @@
                     <v-row justify="center" align="center">
                         <v-col cols="col-4">
                             <v-text-field v-model="searchValue" density="compact" label="Search"
-                                prepend-inner-icon="mdi-magnify" single-line variant="outlined"
+                                prepend-inner-icon="mdi-magnify" single-line :variant="variant"
                                 hide-details></v-text-field>
                         </v-col>
                         <v-col cols="auto">
@@ -55,12 +55,12 @@
                 <v-form fast-fail @submit.prevent="saveData">
                     <v-card flat :title="formTitle">
                     <v-container>
-                        <v-row no-gutters>
+                        <v-row>
                             <v-col cols="12" md="5">
-                                <v-text-field v-model="form.dbName" label="数据库名称"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbName" label="数据库名称"></v-text-field>
                             </v-col>
                             <v-col cols="12" md="5">
-                                <v-text-field v-model="form.dbConstr" label="数据源链接串"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbConstr" label="数据源链接串"></v-text-field>
                             </v-col>
                             <v-col cols="12" md="2">
                                 <v-switch v-model="form.bvalid" 
@@ -70,48 +70,48 @@
                                     hide-details :label="`${form.bvalid == 'Y' ? '启用': '禁用'}`" ></v-switch>
                             </v-col>
                         </v-row>
-                        <v-row no-gutters>
-                            <div><h5>采集信息</h5></div>
+                        <v-row >
+                            <div><h5 class="text-h6 mb-0 pb-0">采集信息</h5></div>
                             <v-divider></v-divider>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbIdEtl" label="采集编号"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbIdEtl" label="采集编号"></v-text-field>
                             </v-col>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbParalEtl" label="并发数"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbParalEtl" label="并发数"></v-text-field>
                                 </v-col>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbUserEtl" label="用户名"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbUserEtl" label="用户名"></v-text-field>
                                 </v-col>    
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbPassEtl" label="密码"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbPassEtl" label="密码"></v-text-field>
                                 </v-col>
                         </v-row>
 
-                        <v-row no-gutters>
-                            <div><h5>数据服务信息</h5></div>
+                        <v-row >
+                            <div><h5 class="text-h6 mb-0 pb-0">数据服务信息</h5></div>
                             <v-divider></v-divider>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbIdDs" label="采集编号"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbIdDs" label="采集编号"></v-text-field>
                             </v-col>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbParalDs" label="并发数"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbParalDs" label="并发数"></v-text-field>
                                 </v-col>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbUserDs" label="用户名"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbUserDs" label="用户名"></v-text-field>
                                 </v-col>
                             <v-col cols="12" md="3">
-                                <v-text-field v-model="form.dbPassDs" label="密码"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbPassDs" label="密码"></v-text-field>
                                 </v-col>
                         </v-row>
 
-                        <v-row no-gutters>
-                            <div><h5>采集扩展信息</h5></div>
+                        <v-row >
+                            <div><h5 class="text-h6 mb-0 pb-0">采集扩展信息</h5></div>
                             <v-divider></v-divider>
                             <v-col cols="12" md="6">
-                                <v-text-field v-model="form.dbStartType" label="启动类型"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbStartType" label="启动类型"></v-text-field>
                             </v-col>
                             <v-col cols="12" md="6">
-                                <v-text-field v-model="form.dbStart" label="启动时间"></v-text-field>
+                                <v-text-field :variant="variant" v-model="form.dbStart" label="启动时间"></v-text-field>
                                 </v-col>
                             <v-col cols="12" md="12">
                                 <v-textarea v-model="form.dbJudgeSql" label="判断脚本" rows="4"></v-textarea>
@@ -124,7 +124,7 @@
                                 </v-col>
                         </v-row>
 
-                        <v-row no-gutters>
+                        <v-row >
                             <v-col cols="12" md="12">
                                 <v-textarea v-model="form.dbRemark" label="备注信息" rows="4"></v-textarea>
                                 </v-col>
@@ -144,96 +144,76 @@
     </div>
 </template>
 
-<script>
+<script setup>
+import { computed, onMounted, ref } from 'vue';
 import axios from 'axios';
 
-export default {
-    data() {
-        return {
-            impdbs: [],
-            //显示模式： show 显示，edit 编辑， add 新建
-            mode: 'show',
-            addMode: false,
-            impdb: {},
-            form: {},
-            searchValue: '',
-            headers: [
+// input style default is outlined
+
+const variant = ref('outlined')
+const impdbs = ref([])
+// 模式： show 显示，edit 编辑， add 新增
+const mode = ref( 'show')
+const form = ref( {})
+const searchValue = ref( '')
+const headers = ref( [
                 { title: "名称", key: "dbName" },
                 { title: "采集编号", key: "dbIdEtl" },
                 { title: "服务编号", key: "dbIdDs" },
                 { title: "操作", value: "actions", align: "center" }
-            ],
-            actions: [
+            ])
+const actions = ref([
                 { text: "详情", value: "show" },
                 { text: "编辑", value: "edit" },
                 { text: "使用场景", value: "scene" },
                 { text: "探索源库", value: "explore" }]
-        }
-    },
-    components: {
-        //  Grid
-    },
+)
 
-    mounted() {
-        this.retrieveImpDB();
-    },
 
-    computed: {
-        filterImpdbs() {
-            const result = this.searchValue === ""
-                ? this.impdbs
-                : this.impdbs.filter(
-                    wo => Object.values(wo).join("").toLocaleLowerCase().indexOf(this.searchValue.toLocaleLowerCase()) !== -1
-                );
-            return result;
-
-        },
-        formTitle() {
-            if (this.mode == 'add') {
+const formTitle = computed(() => {
+            if (mode.value == 'add') {
                 return '新增数据源'
-            } else if (this.mode == 'edit')  {
+            } else if (mode.value == 'edit')  {
                 return '编辑数据源'
             } else {
                 return '数据源详情'
             }
-        }
-    },
-    methods: {
-        retrieveImpDB() {
+        })
+
+        
+const retrieveImpDB = () => {
             axios.get("/maintable/datasource/list")
                 .then(resp => {
-                    this.impdbs = resp.data
+                    impdbs.value = resp.data
                     return resp
                 })
                 .catch(error => {
                     return error
                 });
-        },
+        }
 
-        doAction(id, ctype) {
-            this.dataOp(id, ctype)
-        },
+const doAction = (id, ctype) => {
+            dataOp(id, ctype)
+        }
 
-        dataOp(id, flag) {
-            this.mode = flag
+const dataOp = (id, flag) => {
+            mode.value = flag
             axios.get("/maintable/datasource/detail/" + id)
                 .then(resp => {
-                    this.form = resp.data
+                    form.value = resp.data
                     return resp
                 })
                 .catch(error => {
                     return error
                 });
-        },
+        }
+const addDataSource = () => {
+            mode.value = 'add'
+            form.value = {}
+        }
 
-        addDataSource() {
-            this.mode = 'add'
-            this.form = {}
-        },
-
-        saveData() {
-            console.log(this.mode)
-            if (this.mode == 'add' || this.mode == 'edit') {
+const saveData = () => {
+            if (mode.value == 'add' || mode.value == 'edit') {
                 axios.post('/maintable/datasource/save', {
                     method: 'POST',
                     headers: {
@@ -242,21 +222,17 @@ export default {
                     body: JSON.stringify(this.form)
                 }).then(resp => resp.json());
 
-                this.form = ''
-
+                form.value = ''
                 // get data agtain, it's lazy
-                this.retrieveImpDB()
+                retrieveImpDB()
             }
-        },
-
-        search() {
-            this.impdbs.filters(impdb => {
-                console.log(impdb.props)
-            })
         }
-    }
-}
+
+// load data
+
+onMounted(() => {
+    retrieveImpDB()
+})
 </script>
-
-
-<style></style>
+<style>
+</style>
