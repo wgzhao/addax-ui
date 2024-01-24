26beee380fb05121f909be587e6dceff3a5655ed
wgzhao <wgzhao@gmail.com>
2024-01-24 15:49:49 +0800

    Add request utility functions

---
+++ src/utils/request.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { ref } from "vue";
+
+export function get(url, params) {
+  const data = ref()
+  if (params === undefined ) {
+    axios.get(url).then(res => data.value = res.data).catch(err => {alert("请求(" + url + ")失败:\n" + err.message); console.log(err)})
+  } else {
+    axios.get(url, {
+      params: params,
+    }).then(res => data.value = res.data)
+    .catch(err => {alert("请求(" + url + ")失败:\n" + err.message); console.log(err)})
+  }
+  return data
+}
+
+export function post(url, data) {
+  axios.post(url, data).then(res => alert("请求成功")).catch(err => {alert("请求(" + url + ")失败:\n" + err.message); console.log(err)})
+}
+
+export default {get, post}
