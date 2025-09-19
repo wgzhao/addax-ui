9bfec235c60963573bbef09b937e1bd282dcb6f3
wgzhao <wgzhao@gmail.com>
2025-09-19 17:47:25 +0800

    Refactor code structure for improved readability and maintainability

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import vue from 'eslint-plugin-vue'
+import vuetify from 'eslint-plugin-vuetify'
 
+
+export default [
+  ...vue.configs['flat/base'],
+  ...vuetify.configs['flat/base'],
+]
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-];
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   ...pluginVue.configs["flat/essential"],
+//   {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+// ];
